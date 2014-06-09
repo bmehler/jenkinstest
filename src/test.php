83610cf6617c87b385b84b037fecd7ccbe7b7beb
bmehler <bernhard.mehler@gmail.com>
2014-06-09 15:00:37 +0000

    key jetzt ohne passphrase

--- src/test.php
+++ src/test.php
@@ -5,6 +5,7 @@ echo 'Hoffentlich morgen auch';
 echo 'Es regnet nicht!';
 echo 'Manchmal gibts auch Schnee!';
 echo 'Ich weine!';
-echo 'Wann wirds mal wieder richtig Sommer?";
+echo 'Wann wirds mal wieder richtig Sommer?';
+echo 'OH da hat sich ein Fehler eingeschlichen';
 echo 'Jetzt ist wieder Somme';
 ?>
